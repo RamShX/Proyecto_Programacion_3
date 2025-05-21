ad825e3f3b5df998400fd0b6718826a3242f7fa4
Ramshley P. <ramshleyv@gmail.com>
2025-05-20 23:59:46 -0400

    Agregando el controlador User

--- P0006/Models/ViewModels/QueryViewModels.cs
+++ P0006/Models/ViewModels/QueryViewModels.cs
@@ -4,5 +4,8 @@ namespace P0006.Models.ViewModels
 {
 	public class QueryViewModels
 	{
-	}
+		public int Id { get; set; }
+		public string _Email { get; set; }
+		public int _Edad { get; set; }
+    }
 }
