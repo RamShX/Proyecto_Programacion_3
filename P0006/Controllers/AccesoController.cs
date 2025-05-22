1a5b2c0f0eb7cf925b967894c131cd52b3e5eda9
Ramshley P. <ramshleyv@gmail.com>
2025-05-22 11:12:41 -0400

    Se agregó la parte del login para acceder a sistema y el controlador cerrarSeccion.

---
+++ P0006/Controllers/AccesoController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using P0006.Models;
+
+namespace P0006.Controllers
+{
+    public class AccesoController : Controller
+    {
+        // GET: Acceso
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult Enter(string usuario, string password)
+        {
+            using(var db = new DBMVCEntities()) 
+            {
+                // Select de la tabla USERS donde el email y password coincidan y el estatus sea 1, o sea, activo.
+                var read = from u in db.USERS
+                           where u.Email == usuario
+                           && u.Password == password
+                           && u.idEstatus == 1
+                           select u;
+
+                // Se hace un conteo de los registros que cumplen con la consulta anterior.
+                // Preguntamos si encontramos el registro.
+                if (read.Count() > 0) 
+                {
+                    // Si el usuario existe, se guarda en la session y se retorna 1 a la vista para indicar que el login fue exitoso.
+                    Session["Usuario"] = read.First();
+                    return Content("1");
+                }
+                else
+                {
+                    // Si no encuentra el usuario, retorna un mensaje de error.
+                    return Content("Revisa el usuario y/o password :("); 
+                }
+            }
+        }
+    }
+}
---
+++ P0006/Controllers/CerrarSeccionController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace P0006.Controllers
+{
+    public class CerrarSeccionController : Controller
+    {
+        // GET: CerrarSeccion
+        public ActionResult Logoff()
+        {
+            // Se cierra la sesión del usuario.
+            Session["Usuario"] = null;
+            return RedirectToAction("Login", "Acceso");
+        }
+    }
+}
